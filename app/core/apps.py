3431c64f692b7e3d67e7125a837163d63a25675f
Thorsten Braeumer <tbraeumer@navis-ams.com>
2020-07-20 14:56:19 +0200

    added feature to filter recipes added debugging support (attach debugger)

--- app/core/apps.py
+++ app/core/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
 
+#import ptvsd
 
 class CoreConfig(AppConfig):
     name = 'core'
+
+    #enable debugger to attach to running application (use launch config "Python: Remote Attach")
+    #ptvsd.enable_attach(address=('0.0.0.0', 5678))
+    #ptvsd.wait_for_attach()
--- app/manage.py
+++ app/manage.py
@@ -2,10 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+#import ptvsd
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
+    # comment in the following 2 lines to debug tests in container, then run
+    # 'docker-compose run app python manage.py test' and use launch config "Python: Remote Attach")
+#    ptvsd.enable_attach(address=('0.0.0.0', 5678))
+#    ptvsd.wait_for_attach()
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
